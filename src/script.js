5628578af1963b4e5427b180e578300da7f8a4aa
Hin Chan <32475946+chnchan@users.noreply.github.com>
2018-08-18 16:29:09 -0700

    Done, Remaining: add delete row function, maybe allow name (tho it may screw up the look)

--- src/script.js
+++ src/script.js
@@ -1,43 +1,136 @@
 var entries = document.querySelectorAll(".entries");
 var temp = entries[entries.length-1].getElementsByTagName("input");
+entries[entries.length-1].addEventListener("change", calculate);
+
 
 for (var i = 0; i < temp.length; i++)
 {
-    if (temp[i].type =="text")
-      temp[i].addEventListener("change", addInput);
+  if (temp[i].className == "money")
+  {
+    temp[i].addEventListener("change", addInput);
+    temp[i].addEventListener("change", roundUp2decimal);
+  }
+
+  if (temp[i].className == "itemNum")
+    temp[i].addEventListener("change", roundDown);
 }
 
-function checkForEnter(event)
+temp = document.querySelectorAll(".editable");
+
+for (var i = 0; i < temp.length; i++)
+  temp[i].addEventListener("click", editMode);
+
+temp = document.querySelectorAll(".Box");
+for (var i = 0; i < temp.length; i++)
 {
-  if (event.which == 13)
-   addInput.call(this);
+  temp[i].addEventListener("blur", saveEdit);
+  temp[i].addEventListener("keypress", checkForEnter);
 }
+// ^^ Event Listeners
 
 
 function addInput()
 {
-  // removing eventListener
   var entry = this.parentNode.parentNode;
-  var temp = entry.getElementsByTagName("input");
+  this.removeEventListener("change", addInput);
+  entry.insertAdjacentHTML('beforeend', '<div class="description"><input type="text" placeholder="Item Description"/></div><div class="price"><input class="money" type="number" step="0.01" placeholder="$"/></div><div class="quantity"><input class="itemNum" type="number" step="1" min="1" value="1"/></div><div class="checkboxes"> <input type="checkbox"/> <input type="checkbox"/> <input type="checkbox"/> <input type="checkbox"/> </div>');
+  var entries = document.querySelectorAll(".money");
+  entries[entries.length-1].addEventListener("change", addInput);
+  entries[entries.length-1].addEventListener("change", roundUp2decimal);
+} // only last row will trigger new line
 
-  for (var i = 0; i < temp.length; i++)
-  {
-      if (temp[i].type =="text")
-      {
-        temp[i].removeEventListener("blur", addInput);
-        temp[i].removeEventListener("keypress", checkForEnter);
-      }
-  }
-  //
+// Hide/Show Header Editing Mode
+function editMode()
+{
+  this.classList.add("hide");
+  var input = document.getElementById(this.attributes["target"].value);
+  input.classList.remove("hide");
+  input.select();
+} //hide html, show input
 
-  entry.insertAdjacentHTML('beforeend', '      <div class="description">          <input type="text" placeholder="Item Description"/>        </div>          <div class="price">          <input type="text" placeholder="$"/>        </div>          <div class="quantity">          <input type="text" placeholder="1"/>        </div>          <div class="checkboxes">          <input type="checkbox"/>          <input type="checkbox"/>          <input type="checkbox"/>          <input type="checkbox"/>          <input type="checkbox"/>        </div>');
-
-  var entries = document.querySelectorAll(".entries");
-  var temp = entries[entries.length-1].getElementsByTagName("input");
-
-  for (var i = 0; i < temp.length; i++)
-  {
-      if (temp[i].type =="text")
-        temp[i].addEventListener("change", addInput);
-  }
+function checkForEnter(event)
+{
+  if (event.which == 13)
+   saveEdit.call(this);
 }
+
+function saveEdit()
+{
+  var purpose = document.getElementById(this.attributes["target"].value);
+  if (this.value.trim().length)
+    purpose.innerHTML = this.value;
+  this.classList.add("hide");
+  purpose.classList.remove("hide");
+} // copy input to html
+//
+
+
+// Rounding
+function roundDown()
+{
+  this.value = Math.floor(this.value);
+} // round down ex. 1.5 -> 1
+
+function roundUp2decimal()
+{
+  this.value = Math.round(this.value * 100) / 100;
+} // round to 2nd decimal place
+//
+
+
+//Calculate
+function calculate()
+{
+  var individuals = new Array();
+  individuals[0] = 0.0;
+  individuals[1] = 0.0;
+  individuals[2] = 0.0;
+  individuals[3] = 0.0;
+  var dollars = document.querySelectorAll(".money");
+  var quantities = document.querySelectorAll(".itemNum");
+  var participants = document.querySelectorAll(".checkboxes");
+  var checked = 0, dollarsToAdd = 0.0, totalDollar = 0.0;
+
+  for (var i = 0; i < dollars.length; i++)
+  {
+    for (var j = 0; j < participants[i].children.length; j++)
+    {
+      if (participants[i].children[j].checked)
+        checked += 1;
+    }
+
+    if (checked == 0)
+      checked = 4; // if none of them are checked, it is assumed that everyone is a participant
+    if (dollars[i].value != "")
+      dollarsToAdd = dollars[i].value * quantities[i].value / checked;
+
+    if (checked == 4)
+    {
+      individuals[0] += dollarsToAdd;
+      individuals[1] += dollarsToAdd;
+      individuals[2] += dollarsToAdd;
+      individuals[3] += dollarsToAdd;
+    }
+    else
+    {
+      for (var j = 0; j < participants[i].children.length; j++)
+      {
+        if (participants[i].children[j].checked)
+          individuals[j] += dollarsToAdd;
+      }
+    }
+
+    checked = 0;
+    dollarsToAdd = 0.0;
+  }
+
+  for (var i = 0; i < individuals.length; i++)
+    totalDollar += individuals[i];
+
+  document.getElementById("totalDollar").innerHTML = "Total: $" + totalDollar;
+  document.getElementById("A").innerHTML = "A: $" + individuals[0].toFixed(2);
+  document.getElementById("B").innerHTML = "B: $" + individuals[1].toFixed(2);
+  document.getElementById("C").innerHTML = "C: $" + individuals[2].toFixed(2);;
+  document.getElementById("D").innerHTML = "D: $" + individuals[3].toFixed(2);
+}
+//
